15a674619bcf8d997502b45e7d02fc60f115fa27
bbaranski <bastian.baranski@gmail.com>
2008-09-16 20:03:33 +0000

    Bugfixing install batch file and adding install shell script for Linux.
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@559 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-install-wizard/install.bat
+++ 52n-wps-install-wizard/install.bat
@@ -1,1 +1,1 @@
-java -jar target\52n-wps-webapp-1.0-SNAPSHOT\WEB-INF\lib\52n-wps-install-wizard-1.0-SNAPSHOT.jar 
+java -jar ..\52n-wps-webapp\target\52n-wps-webapp-1.0-rc3-SNAPSHOT\WEB-INF\lib\52n-wps-install-wizard-1.0-rc3-SNAPSHOT.jar 
---
+++ 52n-wps-install-wizard/install.sh
@@ -0,0 +1,2 @@
+java -jar ../52n-wps-webapp/target/52n-wps-webapp-1.0-rc3-SNAPSHOT/WEB-INF/lib/52n-wps-install-wizard-1.0-rc3-SNAPSHOT.jar
+ 
