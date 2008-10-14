ef5f2aa313bc00acff5c773c5cc960663629a9e4
BSC-52n <dev-null@52north.org>
2008-10-14 23:37:14 +0000

    error handling modified
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@571 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-transactional/src/main/java/org/n52/wps/transactional/deploy/IDeployManager.java
+++ 52n-wps-transactional/src/main/java/org/n52/wps/transactional/deploy/IDeployManager.java
@@ -32,22 +32,20 @@ is extensible in terms of processes and data handlers.
 package org.n52.wps.transactional.deploy;
 
 import java.util.Collection;
-import java.util.Map;
 
 import net.opengis.wps.x100.ExecuteDocument;
 
 import org.n52.wps.transactional.request.DeployProcessRequest;
 import org.n52.wps.transactional.request.UndeployProcessRequest;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
 public interface IDeployManager {
 
 	
 	boolean unDeployProcess(UndeployProcessRequest request) throws Exception;
-	boolean containsProcess(String processID);
-	Collection<String> getAllProcesses();
-	Document invoke(ExecuteDocument payload, String algorithmID);
+	boolean containsProcess(String processID) throws Exception;
+	Collection<String> getAllProcesses() throws Exception;
+	Document invoke(ExecuteDocument payload, String algorithmID) throws Exception;
 	boolean deployProcess(DeployProcessRequest request) throws Exception;
 	
 }
