b27692a6725c191bd0acd02f7625306cbc7221f2
BSC-52n <dev-null@52north.org>
2008-10-14 10:54:09 +0000

    freaking headers for wps-t extension
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@569 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-transactional/src/main/java/org/n52/wps/transactional/algorithm/OutputParser.java
+++ 52n-wps-transactional/src/main/java/org/n52/wps/transactional/algorithm/OutputParser.java
@@ -1,3 +1,34 @@
+/***************************************************************
+ This implementation provides a framework to publish processes to the
+web through the  OGC Web Processing Service interface. The framework 
+is extensible in terms of processes and data handlers. 
+
+ Copyright (C) 2006 by con terra GmbH
+
+ Authors: 
+	Bastian Schaeffer, Institute for Geoinformatics, Muenster, Germany
+
+ Contact: Albert Remke, con terra GmbH, Martin-Luther-King-Weg 24,
+ 48155 Muenster, Germany, 52n@conterra.de
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program (see gnu-gpl v2.txt); if not, write to
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA or visit the web page of the Free
+ Software Foundation, http://www.fsf.org.
+
+ ***************************************************************/
+
+
 package org.n52.wps.transactional.algorithm;
 import net.opengis.wps.x100.OutputDataType;
 import net.opengis.wps.x100.OutputDescriptionType;
