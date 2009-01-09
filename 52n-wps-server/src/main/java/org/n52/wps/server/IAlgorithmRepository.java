3a258024257513cf0bd83b3df0cd4fa96a1dd9b2
BSC-52n <dev-null@52north.org>
2009-01-09 09:59:36 +0000

    Changes from WPS 2.0 branch
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@641 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-server/src/main/java/org/n52/wps/server/IAlgorithmRepository.java
+++ 52n-wps-server/src/main/java/org/n52/wps/server/IAlgorithmRepository.java
@@ -1,3 +1,36 @@
+/***************************************************************
+This implementation provides a framework to publish processes to the
+web through the  OGC Web Processing Service interface. The framework 
+is extensible in terms of processes and data handlers. It is compliant 
+to the WPS version 0.4.0 (OGC 05-007r4). 
+
+Copyright (C) 2009 by con terra GmbH
+
+Authors: 
+	Bastian Schäffer, University of Muenster
+	Theodor Foerster, ITC
+
+
+Contact: Albert Remke, con terra GmbH, Martin-Luther-King-Weg 24,
+48155 Muenster, Germany, 52n@conterra.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program (see gnu-gpl v2.txt); if not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA or visit the web page of the Free
+Software Foundation, http://www.fsf.org.
+
+***************************************************************/
+
 package org.n52.wps.server;
 
 
--- 52n-wps-server/src/main/java/org/n52/wps/server/RepositoryManager.java
+++ 52n-wps-server/src/main/java/org/n52/wps/server/RepositoryManager.java
@@ -1,37 +1,39 @@
 /***************************************************************
- This implementation provides a framework to publish processes to the
+This implementation provides a framework to publish processes to the
 web through the  OGC Web Processing Service interface. The framework 
 is extensible in terms of processes and data handlers. It is compliant 
 to the WPS version 0.4.0 (OGC 05-007r4). 
 
- Copyright (C) 2006 by con terra GmbH
+Copyright (C) 2009 by con terra GmbH
 
- Authors: 
-	Bastian Schäffer, Institute for geoinformatics, University of
-Muenster, Germany
+Authors: 
+	Bastian Schäffer, University of Muenster
 
 
- Contact: Albert Remke, con terra GmbH, Martin-Luther-King-Weg 24,
- 48155 Muenster, Germany, 52n@conterra.de
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
+Contact: Albert Remke, con terra GmbH, Martin-Luther-King-Weg 24,
+48155 Muenster, Germany, 52n@conterra.de
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
 
- You should have received a copy of the GNU General Public License
- along with this program (see gnu-gpl v2.txt); if not, write to
- the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- Boston, MA  02111-1307, USA or visit the web page of the Free
- Software Foundation, http://www.fsf.org.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
- Created on: 23.05.2007
- ***************************************************************/
+You should have received a copy of the GNU General Public License
+along with this program (see gnu-gpl v2.txt); if not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA or visit the web page of the Free
+Software Foundation, http://www.fsf.org.
+
+***************************************************************/
+
+
 package org.n52.wps.server;
+
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -153,5 +155,27 @@ public class RepositoryManager {
 		}
 		return false;
 	}
+	
+	public IAlgorithmRepository getRepositoryForAlgorithm(String algorithmName){
+		for(IAlgorithmRepository repository : repositories){
+			if(repository.containsAlgorithm(algorithmName)){
+				return repository;
+			}
+		}
+		return null;
+	}
+	
+	public Class getInputDataTypeForAlgorithm(String algorithmIdentifier, String inputIdentifier){
+		IAlgorithm algorithm = getAlgorithm(algorithmIdentifier);
+		return algorithm.getInputDataType(inputIdentifier);
+		
+	}
+	
+	public Class getOutputDataTypeForAlgorithm(String algorithmIdentifier, String inputIdentifier){
+		IAlgorithm algorithm = getAlgorithm(algorithmIdentifier);
+		return algorithm.getOutputDataType(inputIdentifier);
+		
+	}
+	
 
 }
