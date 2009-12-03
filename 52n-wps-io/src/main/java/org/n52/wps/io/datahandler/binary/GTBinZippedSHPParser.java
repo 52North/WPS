8173252887f3fa7b8affc0cd304cf63aa310ba9b
BSC-52n <dev-null@52north.org>
2009-12-03 16:34:45 +0000

    shp parser
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@1010 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-io/src/main/java/org/n52/wps/io/datahandler/binary/GTBinZippedSHPParser.java
+++ 52n-wps-io/src/main/java/org/n52/wps/io/datahandler/binary/GTBinZippedSHPParser.java
@@ -2,6 +2,8 @@ package org.n52.wps.io.datahandler.binary;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -19,64 +21,8 @@ import org.n52.wps.io.datahandler.xml.AbstractXMLParser;
 import org.w3c.dom.DOMException;
 import org.xml.sax.SAXException;
 
-public class GTBinZippedSHPParser extends AbstractXMLParser {
-	/**
-	 * @throws RuntimeException
-	 *             If an error occurs while parsing the sent XML, unzipping the
-	 *             zipped shapefile or the feature collection cannot be obtained
-	 *             from the shapefile
-	 * @see org.n52.wps.io.datahandler.xml.AbstractXMLParser#parseXML(java.lang.String)
-	 */
-	@Override
-	public IData parseXML(String xml) throws RuntimeException {
-		return parseXML(new ByteArrayInputStream(xml.getBytes()));
-	}
-
-	/**
-	 * @throws RuntimeException
-	 *             If an error occurs while parsing the sent XML, unzipping the
-	 *             zipped shapefile or the feature collection cannot be obtained
-	 *             from the shapefile
-	 * @see org.n52.wps.io.datahandler.xml.AbstractXMLParser#parseXML(java.io.InputStream)
-	 */
-	@Override
-	public IData parseXML(InputStream stream) throws RuntimeException {
-		try {
-			// Unzip and obtain the data binding
-			File tmp = IOUtils.writeBase64XMLToFile(stream, "zip");
-			File shp = IOUtils.unzip(tmp, "shp");
-
-			if (shp == null) {
-				throw new RuntimeException(
-						"Cannot find a shapefile inside the zipped file.");
-			}
-
-			DataStore store = new ShapefileDataStore(shp.toURI().toURL());
-			FeatureCollection features = store.getFeatureSource(
-					store.getTypeNames()[0]).getFeatures();
-			shp.delete();
-			return new GTVectorDataBinding(features);
-		} catch (ParserConfigurationException e) {
-			throw new RuntimeException(
-					"Cannot initialize parser for XML input", e);
-		} catch (SAXException e) {
-			throw new RuntimeException("Cannot parse XML input", e);
-		} catch (IOException e) {
-			throw new RuntimeException("An error has occurred while obtaining "
-					+ "the feature collection from the sent file", e);
-		} catch (DOMException e) {
-			throw new RuntimeException("Cannot get data from the XML input", e);
-		} catch (TransformerException e) {
-			throw new RuntimeException("Cannot get data from the XML input", e);
-		}
-	}
-
-	@Override
-	public Class<?>[] getSupportedInternalOutputDataType() {
-		return new Class<?>[] { GTVectorDataBinding.class };
-	}
-
-	/**
+public class GTBinZippedSHPParser extends AbstractGTBinZippedSHPParser {
+		/**
 	 * @throws RuntimeException
 	 *             if an error occurs while writing the stream to disk or
 	 *             unzipping the written file
@@ -85,19 +31,34 @@ public class GTBinZippedSHPParser extends AbstractXMLParser {
 	@Override
 	public IData parse(InputStream input) throws RuntimeException {
 		try {
-			File zipped = IOUtils.writeBase64ToFile(input, "zip");
-			File shp = IOUtils.unzip(zipped, "shp");
-
-			if (shp == null) {
-				throw new RuntimeException(
-						"Cannot find a shapefile inside the zipped file.");
+			String fileName = "tempfile" + System.currentTimeMillis()+".zip";
+			File tempFile = new File(fileName);
+			try {
+				FileOutputStream outputStream = new FileOutputStream(tempFile);
+				byte buf[] = new byte[4096];
+				int len;
+				while ((len = input.read(buf)) > 0) {
+					outputStream.write(buf, 0, len);
+				}
+				outputStream.close();
+				input.close();
+			} catch (FileNotFoundException e) {
+				System.gc();
+				tempFile.delete();
+				throw new RuntimeException(e);
+			} catch (IOException e1) {
+				System.gc();
+				tempFile.delete();
+				throw new RuntimeException(e1);
 			}
-
+			File shp = IOUtils.unzip(tempFile, "shp");
 			DataStore store = new ShapefileDataStore(shp.toURI().toURL());
 			FeatureCollection features = store.getFeatureSource(
 					store.getTypeNames()[0]).getFeatures();
-			zipped.delete();
-			shp.delete();
+			System.gc();
+			tempFile.delete();
+			
+			
 			
 			return new GTVectorDataBinding(features);
 		} catch (IOException e) {
@@ -106,29 +67,15 @@ public class GTBinZippedSHPParser extends AbstractXMLParser {
 		}
 	}
 
-	@Override
-	public String[] getSupportedFormats() {
-		return new String[] { IOHandler.MIME_TYPE_ZIPPED_SHP };
-	}
-
-	@Override
-	public String[] getSupportedSchemas() {
-		return new String[] {};
-	}
+	
 
 	@Override
 	public boolean isSupportedEncoding(String encoding) {
-		if(encoding.equals(IOHandler.ENCODING_BASE64)){
-			return true;
-		}
 		if(encoding.equals(IOHandler.DEFAULT_ENCODING)){
 			return true;
 		}
 		return false;
 	}
 
-	@Override
-	public boolean isSupportedSchema(String schema) {
-		return schema == null;
-	}
+	
 }
