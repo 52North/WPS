2cf9a5dcf941d7c21b2a69a111bf3df778ce6df6
BSC-52n <dev-null@52north.org>
2009-12-04 17:14:59 +0000

    Abstract shp parser added
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@1013 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-io/src/main/java/org/n52/wps/io/datahandler/binary/AbstractGTBinZippedSHPParser.java
@@ -0,0 +1,79 @@
+package org.n52.wps.io.datahandler.binary;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.feature.FeatureCollection;
+import org.n52.wps.io.IOHandler;
+import org.n52.wps.io.IOUtils;
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
+import org.n52.wps.io.datahandler.xml.AbstractXMLParser;
+import org.w3c.dom.DOMException;
+import org.xml.sax.SAXException;
+
+public abstract class  AbstractGTBinZippedSHPParser extends AbstractXMLParser {
+	/**
+	 * @throws RuntimeException
+	 *             If an error occurs while parsing the sent XML, unzipping the
+	 *             zipped shapefile or the feature collection cannot be obtained
+	 *             from the shapefile
+	 * @see org.n52.wps.io.datahandler.xml.AbstractXMLParser#parseXML(java.lang.String)
+	 */
+	@Override
+	public IData parseXML(String xml) throws RuntimeException {
+		return parseXML(new ByteArrayInputStream(xml.getBytes()));
+	}
+
+	/**
+	 * @throws RuntimeException
+	 *             If an error occurs while parsing the sent XML, unzipping the
+	 *             zipped shapefile or the feature collection cannot be obtained
+	 *             from the shapefile
+	 * @see org.n52.wps.io.datahandler.xml.AbstractXMLParser#parseXML(java.io.InputStream)
+	 */
+	
+	@Override
+	public IData parseXML(InputStream stream) throws RuntimeException {
+		return parse(stream);
+	}
+
+	@Override
+	public Class<?>[] getSupportedInternalOutputDataType() {
+		return new Class<?>[] { GTVectorDataBinding.class };
+	}
+
+	/**
+	 * @throws RuntimeException
+	 *             if an error occurs while writing the stream to disk or
+	 *             unzipping the written file
+	 * @see org.n52.wps.io.IParser#parse(java.io.InputStream)
+	 */
+	@Override
+	public abstract IData parse(InputStream input);
+
+	@Override
+	public String[] getSupportedFormats() {
+		return new String[] { IOHandler.MIME_TYPE_ZIPPED_SHP };
+	}
+
+	@Override
+	public String[] getSupportedSchemas() {
+		return new String[] {};
+	}
+
+	@Override
+	public abstract boolean isSupportedEncoding(String encoding);
+
+	@Override
+	public boolean isSupportedSchema(String schema) {
+		return schema == null;
+	}
+}
