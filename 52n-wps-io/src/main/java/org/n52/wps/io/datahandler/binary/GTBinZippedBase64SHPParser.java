72eef6696318e0310e9d51af6d7c77b56735cdf1
BSC-52n <dev-null@52north.org>
2009-12-05 13:48:41 +0000

    new shp parser added
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@1014 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-io/src/main/java/org/n52/wps/io/datahandler/binary/GTBinZippedBase64SHPParser.java
@@ -0,0 +1,68 @@
+package org.n52.wps.io.datahandler.binary;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.feature.FeatureCollection;
+import org.n52.wps.io.IOHandler;
+import org.n52.wps.io.IOUtils;
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
+import org.n52.wps.io.datahandler.xml.AbstractXMLParser;
+import org.w3c.dom.DOMException;
+import org.xml.sax.SAXException;
+
+public class GTBinZippedBase64SHPParser extends AbstractGTBinZippedSHPParser {
+	
+
+	
+
+	/**
+	 * @throws RuntimeException
+	 *             if an error occurs while writing the stream to disk or
+	 *             unzipping the written file
+	 * @see org.n52.wps.io.IParser#parse(java.io.InputStream)
+	 */
+	@Override
+	public IData parse(InputStream input) throws RuntimeException {
+		try {
+			File zipped = IOUtils.writeBase64ToFile(input, "zip");
+			File shp = IOUtils.unzip(zipped, "shp");
+
+			if (shp == null) {
+				throw new RuntimeException(
+						"Cannot find a shapefile inside the zipped file.");
+			}
+
+			DataStore store = new ShapefileDataStore(shp.toURI().toURL());
+			FeatureCollection features = store.getFeatureSource(
+					store.getTypeNames()[0]).getFeatures();
+			zipped.delete();
+			shp.delete();
+			
+			return new GTVectorDataBinding(features);
+		} catch (IOException e) {
+			throw new RuntimeException(
+					"An error has occurred while accessing provided data", e);
+		}
+	}
+
+	
+
+	@Override
+	public boolean isSupportedEncoding(String encoding) {
+		if(encoding.equals(IOHandler.ENCODING_BASE64)){
+			return true;
+		}
+		return false;
+	}
+
+	
+}
