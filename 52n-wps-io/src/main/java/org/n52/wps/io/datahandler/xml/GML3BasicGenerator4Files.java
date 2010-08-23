676964fc5e143bf37f94bbc815a109f53d8422c2
BSC-52n <dev-null@52north.org>
2010-08-23 20:28:50 +0000

    added parsers for generating shp file for use with ags and vice versa
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@1386 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-io/src/main/java/org/n52/wps/io/datahandler/xml/GML3BasicGenerator4Files.java
@@ -0,0 +1,82 @@
+package org.n52.wps.io.datahandler.xml;
+
+import java.io.OutputStream;
+import java.io.Writer;
+
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
+import org.n52.wps.io.data.binding.complex.GenericFileDataBinding;
+import org.w3c.dom.Node;
+
+public class GML3BasicGenerator4Files extends GML2BasicGenerator{
+
+	
+	public void write(IData outputData, Writer writer) {
+		if(outputData instanceof GTVectorDataBinding){
+			new GML3BasicGenerator().write(outputData, writer);
+		}else{
+			if (outputData instanceof GenericFileDataBinding) {
+				//convert and recall write
+				GTVectorDataBinding vectorBinding = ((GenericFileDataBinding)outputData).getPayload().getAsGTVectorDataBinding();
+				write(vectorBinding, writer);
+			}
+			else {
+				throw new RuntimeException("GML3BasicGenerator4Files does not support incoming datatype");
+			}
+		}
+	}
+	
+	
+	public Node generateXML(IData outputData, String schema) {
+		
+		if(outputData instanceof GTVectorDataBinding){
+			return new GML3BasicGenerator().generateXML(outputData, schema);
+		}else{
+			if (outputData instanceof GenericFileDataBinding) {
+				//convert and recall generateXML
+				GTVectorDataBinding vectorBinding = ((GenericFileDataBinding)outputData).getPayload().getAsGTVectorDataBinding();
+				return generateXML(vectorBinding, schema);
+			}
+			else {
+				throw new RuntimeException("GML3BasicGenerator4Files does not support incoming datatype");
+			}
+		}
+	}
+	
+	public OutputStream generate(IData outputData) {
+		
+		if(outputData instanceof GTVectorDataBinding){
+			return new GML3BasicGenerator().generate(outputData);
+		}else{
+			if (outputData instanceof GenericFileDataBinding) {
+				//convert and recall generate
+				GTVectorDataBinding vectorBinding = ((GenericFileDataBinding)outputData).getPayload().getAsGTVectorDataBinding();
+				return generate(vectorBinding);
+			}
+			else {
+				throw new RuntimeException("GML3BasicGenerator4Files does not support incoming datatype");
+			}
+		}
+	}
+	
+	public void writeToStream(IData outputData, OutputStream os) {
+		
+		if(outputData instanceof GTVectorDataBinding){
+			new GML3BasicGenerator().writeToStream(outputData, os);
+		}else{
+			if (outputData instanceof GenericFileDataBinding) {
+				//convert and recall writeToStream
+				GTVectorDataBinding vectorBinding = ((GenericFileDataBinding)outputData).getPayload().getAsGTVectorDataBinding();
+				writeToStream(vectorBinding, os);
+			}
+			else {
+				throw new RuntimeException("GML3BasicGenerator4Files does not support incoming datatype");
+			}
+		}
+	}
+	
+	public Class[] getSupportedInternalInputDataType() {
+		Class[] supportedClasses = {GenericFileDataBinding.class};
+		return supportedClasses;
+	}
+}
