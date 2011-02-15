e9d06e12bbf33f1bef401834040fd9b368d0b533
matthias-mueller <matthias_mueller@tu-dresden.de>
2011-02-15 16:30:36 +0000

    - new classes for python scripting
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@1600 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-python/src/main/java/org/n52/wps/python/PythonProcessRepository.java
@@ -0,0 +1,204 @@
+/***************************************************************
+Copyright © 2011 52°North Initiative for Geospatial Open Source Software GmbH
+
+ Author: Matthias Mueller, TU Dresden
+ 
+ Contact: Andreas Wytzisk, 
+ 52°North Initiative for Geospatial Open Source SoftwareGmbH, 
+ Martin-Luther-King-Weg 24,
+ 48155 Muenster, Germany, 
+ info@52north.org
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; even without the implied WARRANTY OF
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program (see gnu-gpl v2.txt). If not, write to
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA 02111-1307, USA or visit the Free
+ Software Foundation’s web page, http://www.fsf.org.
+
+ ***************************************************************/
+
+package org.n52.wps.python;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+import org.n52.wps.PropertyDocument.Property;
+import org.n52.wps.commons.WPSConfig;
+import org.n52.wps.server.IAlgorithm;
+import org.n52.wps.server.IAlgorithmRepository;
+import org.n52.wps.server.feed.movingcode.MovingCodeObject;
+import org.n52.wps.server.request.ExecuteRequest;
+
+public class PythonProcessRepository implements IAlgorithmRepository {
+	
+	private static Logger LOGGER = Logger.getLogger(PythonProcessRepository.class);
+	
+	private static final String PROPERTY_PROCESS_INVENTORY_DIR = "PROCESS_INVENTORY_DIR";
+	private static final String PROPERTY_CONTAINER_URN = "CONTAINER_URN";
+	private static final String PROPERTY_BACKEND_URN = "BACKEND_URN";
+	private static final String PROPERTY_WORKSPACEBASE = "WORKSPACEBASE";
+	
+	
+	private HashMap<String, MovingCodeObject> registeredAlgorithms;
+	private URI[] supportedContainers;
+	private URI[] supportedBackends;
+	private File workspaceBase = null;
+	private File inventoryDir = null;
+	
+	
+	public PythonProcessRepository (){
+		LOGGER.info("Initializing Python Process Repository ...");
+		
+		//initialize local variables
+		registeredAlgorithms = new HashMap<String, MovingCodeObject>();
+		try{
+			loadConfiguration();
+			loadLocalProcesses();
+			loadFeedProcesses();
+		} catch (Exception e){
+			LOGGER.error("Could not initialize PythonProcessRepository.");
+		}
+	}
+	
+	private void loadConfiguration() throws Exception{
+		Property[] props = WPSConfig.getInstance().getPropertiesForRepositoryClass(this.getClass().getCanonicalName());
+		ArrayList<URI> containerList = new ArrayList<URI>();
+		ArrayList<URI> backendList = new ArrayList<URI>();
+		
+		for(Property currentProp : props){
+			try{
+				if (currentProp.getActive()){
+					String propName = currentProp.getName();
+					if (propName.equalsIgnoreCase(PROPERTY_PROCESS_INVENTORY_DIR)){
+						inventoryDir = new File (currentProp.getStringValue());
+					}
+					if (propName.equalsIgnoreCase(PROPERTY_WORKSPACEBASE)){
+						workspaceBase = new File (currentProp.getStringValue());
+					}
+					if (propName.equalsIgnoreCase(PROPERTY_BACKEND_URN)){
+						backendList.add(new URI (currentProp.getStringValue()));
+					}
+					if (propName.equalsIgnoreCase(PROPERTY_CONTAINER_URN)){
+						containerList.add(new URI (currentProp.getStringValue()));
+					}
+				}
+			} catch (URISyntaxException e){
+				LOGGER.error("Invalid URI - offending item is " + currentProp.getStringValue());
+			}
+		}
+		
+		supportedContainers = containerList.toArray(new URI[0]);
+		supportedBackends = backendList.toArray(new URI[0]);
+		containerList = null;
+		backendList = null;
+		
+	}
+	
+	private void loadLocalProcesses(){
+		String[] describeProcessFiles = retrieveProcessDescriptions(inventoryDir);
+		registeredAlgorithms = new HashMap<String, MovingCodeObject>();
+		
+		// create new MovingCodeObjects
+		for (String currentFileName : describeProcessFiles){
+			File currentFile = new File (inventoryDir.getAbsolutePath() + File.separator + currentFileName);
+			MovingCodeObject currentMCO = new MovingCodeObject(currentFile, inventoryDir);
+			if (isSupportedScript(currentMCO)){
+				registeredAlgorithms.put(currentMCO.getProcessID(), currentMCO);
+			} else {
+				LOGGER.info(currentMCO.getProcessID() + "is not supported by this repository. - Dropping algorithm.");
+				currentMCO = null;
+			}
+		}
+	}
+	
+	private void loadFeedProcesses(){
+		// TODO: implement
+	}
+	
+	private boolean isSupportedScript(MovingCodeObject mco){
+		boolean rightContainer = false;
+		boolean rightBackends = false;
+		for (URI currentContainer : supportedContainers){
+			if (mco.isSupportedContainer(currentContainer)){
+				rightContainer = true;
+			}
+		}
+		
+		if (mco.isSupportedRuntimeEnvironment(supportedBackends)){
+			rightBackends = true;
+		}
+		
+		return (rightContainer && rightBackends);
+	}
+	
+	private static String[] retrieveProcessDescriptions(File directory){
+		String[] describeProcessFiles = directory.list(new FilenameFilter() {
+		    public boolean accept(File d, String name) {
+		       return name.endsWith(".xml");
+		    }
+		});
+		return describeProcessFiles;
+	}
+	
+	/**
+	 * Checks if a given processID exists 
+	 * @param processID
+	 * @return
+	*/
+	public boolean containsAlgorithm(String processID) {
+		if(registeredAlgorithms.containsKey(processID)){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns an IAlgorithm through GenericAGSProcessDelegator
+	 * @param processID
+	 * @return
+	*/
+	public IAlgorithm getAlgorithm(String processID, ExecuteRequest executeRequest) {
+		if(!containsAlgorithm(processID)){
+			throw new RuntimeException("Could not allocate Process " + processID);
+		}
+		try {
+			return new PythonScriptDelegator(registeredAlgorithms.get(processID), workspaceBase);
+		} catch (IOException e) {
+			LOGGER.error(processID + ": Instantiation failed!");
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public Collection<String> getAlgorithmNames() {
+		return registeredAlgorithms.keySet();
+	}
+	
+	public Collection<IAlgorithm> getAlgorithms() {
+		Collection<IAlgorithm> algorithms = new ArrayList<IAlgorithm>(registeredAlgorithms.size());
+		for(String processID : registeredAlgorithms.keySet()){
+			IAlgorithm algorithm = getAlgorithm(processID, null);
+			if(algorithm!=null){
+				algorithms.add(algorithm);
+			}
+		}
+		return algorithms;
+	}
+	
+}
---
+++ 52n-wps-python/src/main/java/org/n52/wps/python/PythonScriptDelegator.java
@@ -0,0 +1,212 @@
+/***************************************************************
+Copyright © 2011 52°North Initiative for Geospatial Open Source Software GmbH
+
+ Author: Matthias Mueller, TU Dresden
+ 
+ Contact: Andreas Wytzisk, 
+ 52°North Initiative for Geospatial Open Source SoftwareGmbH, 
+ Martin-Luther-King-Weg 24,
+ 48155 Muenster, Germany, 
+ info@52north.org
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; even without the implied WARRANTY OF
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program (see gnu-gpl v2.txt). If not, write to
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA 02111-1307, USA or visit the Free
+ Software Foundation’s web page, http://www.fsf.org.
+
+ ***************************************************************/
+
+package org.n52.wps.python;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import net.opengis.wps.x100.ProcessDescriptionType;
+
+import org.apache.log4j.Logger;
+import org.n52.wps.io.data.GenericFileData;
+import org.n52.wps.io.data.GenericFileDataConstants;
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.data.binding.complex.GenericFileDataBinding;
+import org.n52.wps.server.IAlgorithm;
+import org.n52.wps.server.feed.movingcode.AlgorithmParameterType;
+import org.n52.wps.server.feed.movingcode.CommandLineParameter;
+import org.n52.wps.server.feed.movingcode.MovingCodeUtils;
+import org.n52.wps.server.feed.movingcode.MovingCodeObject;
+
+public class PythonScriptDelegator implements IAlgorithm{
+	private static final String COMMAND = "cmd /c";
+	
+	private static Logger LOGGER = Logger.getLogger(PythonScriptDelegator.class);
+	
+	private CommandLineParameter[] scriptParameters;
+	
+	private MovingCodeObject mco;
+	protected List<String> errors;
+
+	public PythonScriptDelegator(MovingCodeObject mco, File workspaceBase) throws IOException{
+		this.errors = new ArrayList<String>();
+		this.mco = mco.createChild(workspaceBase);
+		this.scriptParameters = new CommandLineParameter[mco.getParameters().size()];
+	}
+	
+	
+	public Map<String, IData> run(Map<String, List<IData>> inputData) {
+		File instanceWorkspace = new File(mco.getInstanceWorkspace().getAbsolutePath());
+		String instanceExecutable = instanceWorkspace + File.separator + mco.getAlgorithmURL().getPublicPath();
+		
+		List<AlgorithmParameterType> params = mco.getParameters();
+		
+		HashMap<String, String> outputs = new HashMap<String,String>();
+		
+		for (AlgorithmParameterType currentParam : params){
+			String wpsInputID = currentParam.getWpsInputID();
+			String wpsOutputID = currentParam.getWpsOutputID();
+			String prefixString = currentParam.getPrefixString();
+			int positionID = currentParam.getPositionID().intValue();
+			
+			CommandLineParameter cmdParam = null;
+			
+			// input parameters
+			if(wpsInputID != null){
+				if(inputData.containsKey(wpsInputID)){
+					//open the IData list and iterate through it
+					List<IData> dataItemList = inputData.get(wpsInputID);
+					Iterator<IData> it = dataItemList.iterator();
+					//create CommanLineParameter Object
+					cmdParam = new CommandLineParameter(currentParam.getPrefixString(), currentParam.getSuffixString(), currentParam.getSeparatorString());
+					while (it.hasNext()){
+						IData currentItem = it.next();
+						// load as file and add to CommanLineParameter
+						cmdParam.addValue((MovingCodeUtils.loadSingleDataItem(currentItem, instanceWorkspace)));
+					}
+				}
+			} 
+			
+			// output only parameters !!ONLY SINGLE OUTPUT ITEMS SUPPORTED BY WPS!!
+			else if (wpsOutputID != null){
+				// create CommanLineParameter Object
+				cmdParam = new CommandLineParameter(currentParam.getPrefixString(), currentParam.getSuffixString(), currentParam.getSeparatorString());
+				
+				// retrieve the default mimeType
+				String mimeType = mco.getDefaultMimeType(wpsOutputID);
+				
+				// prepare output filename
+				String extension = GenericFileDataConstants.mimeTypeFileTypeLUT().get(mimeType);
+				String fileName = System.currentTimeMillis() + "." + extension;
+				fileName = instanceWorkspace.getAbsolutePath() + File.separator + fileName;
+				cmdParam.addValue(fileName);
+			}
+			
+			//prepare the output - files only
+			if (wpsOutputID != null){
+				String fileName = cmdParam.getAsPlainString();
+				outputs.put(wpsOutputID, fileName);
+			}
+			
+			// create a new parameter in the Parameter Array
+			scriptParameters[positionID] = cmdParam;
+		}
+		
+		// initialize execution
+		LOGGER.info("Executing CommandLine Algorithm " + instanceExecutable + " . Parameter array contains " + scriptParameters.length + " parameters.");
+		
+		// build the execution command
+		String command = COMMAND + " " + instanceExecutable;
+		for (CommandLineParameter currentParam : scriptParameters){
+			command = command + " " + currentParam.getAsCommandString();
+		}
+		
+		// execute
+		LOGGER.info("Executing " + command);
+		executeScript(command, instanceWorkspace);
+		
+		//create the output - files only
+		HashMap<String, IData> result = new HashMap<String, IData>();
+		for (String wpsOutputID : outputs.keySet()){
+			// create File object
+			File currentFile = new File (outputs.get(wpsOutputID));
+			GenericFileData outputFileData;
+			try {
+				// create the GenericFileData object
+				outputFileData = new GenericFileData(currentFile, mco.getDefaultMimeType(wpsOutputID));
+				// put result on output map
+				result.put(wpsOutputID, new GenericFileDataBinding(outputFileData));
+			} catch (FileNotFoundException e) {
+				LOGGER.error("Could not read output file: " + outputs.get(wpsOutputID));
+				e.printStackTrace();
+			} catch (IOException e) {
+				LOGGER.error("Could not create output file from: " + outputs.get(wpsOutputID));
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public ProcessDescriptionType getDescription() {
+		return mco.getProcessDescription();
+	}
+
+	public String getWellKnownName() {
+		return mco.getProcessID();
+	}
+
+	public boolean processDescriptionIsValid() {
+		return mco.getProcessDescription().validate();
+	}
+
+	public Class getInputDataType(String id) {
+		return MovingCodeUtils.getInputDataType(mco, id);
+	}
+
+	public Class getOutputDataType(String id) {
+		return MovingCodeUtils.getOutputDataType(mco, id);
+	}
+	
+	private void executeScript(String command, File workspaceDir) //throws Exception
+	{
+		try {
+			Process p = Runtime.getRuntime().exec(command, null, workspaceDir);
+			p.waitFor();
+			if (p.exitValue() == 0){
+				LOGGER.info("Successfull termination of command:\n" + command);
+			}
+			else {
+				LOGGER.error("Abnormal termination of command:\n" + command);
+				LOGGER.error("Errorlevel / Exit Value: " + p.exitValue());
+				throw new IOException();
+			}
+		}
+		catch (IOException e) {
+			LOGGER.error("Error executing command:\n" + command);
+			e.printStackTrace();
+			//throw new Exception();
+		} catch (InterruptedException e) {
+			LOGGER.error("Execution interrupted! Command was:\n" + command);
+			e.printStackTrace();
+			//throw new Exception();
+		}
+	}
+	
+}
