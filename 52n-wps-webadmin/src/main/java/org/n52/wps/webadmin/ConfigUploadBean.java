f8227f582180b8057d1397926f7960e18d6c4e43
BSC-52n <dev-null@52north.org>
2011-05-11 17:36:58 +0000

    functionality for proces description upload adeed
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@1740 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-webadmin/src/main/java/org/n52/wps/webadmin/ConfigUploadBean.java
+++ 52n-wps-webadmin/src/main/java/org/n52/wps/webadmin/ConfigUploadBean.java
@@ -62,7 +62,7 @@ public class ConfigUploadBean {
 	private static transient Logger LOGGER = Logger
 			.getLogger(ConfigUploadBean.class);
 
-	private String savePath, filepath, filename;
+	private String savePath, filepath, filename, xmlFilepath, xmlFilename;
 	private final String filenamePrefix = "userConf_";
 
 	public String getFilenamePrefix() {
@@ -85,6 +85,26 @@ public class ConfigUploadBean {
 				filename = filepath.substring(pos + 1);
 			} else {
 				filename = filepath;
+			}
+		}
+	}
+	
+	private void setXMLFilename(String s) {
+		if (s == null) {
+			return;
+		}
+
+		int pos = s.indexOf("filename=\"");
+		if (pos != -1) {
+			xmlFilepath = s.substring(pos + 10, s.length() - 1);
+			// Windows browsers include the full path on the client
+			// But Linux/Unix and Mac browsers only send the filename
+			// test if this is from a Windows browser
+			pos = xmlFilepath.lastIndexOf("\\");
+			if (pos != -1) {
+				xmlFilename = xmlFilepath.substring(pos + 1);
+			} else {
+				xmlFilename = xmlFilepath;
 			}
 		}
 	}
@@ -113,6 +133,7 @@ public class ConfigUploadBean {
 		String realSavePath = "";
 		while (i != -1) {
 			String newLine = new String(line, 0, i);
+			System.out.println(newLine);
 			if (newLine.contains("processName")) {
 				// we set a flag to know that the next string is the fully
 				// qualified classname
@@ -141,6 +162,63 @@ public class ConfigUploadBean {
 			}
 
 			if (newLine.startsWith("Content-Disposition: form-data; name=\"")) {
+				if (newLine.contains("processDescriptionFile")) {
+					if (newLine.indexOf("filename=\"") != -1) {
+						setXMLFilename(new String(line, 0, i - 2));
+						if (xmlFilename == null) {
+							return;
+						}
+
+						// this is the file content
+						i = in.readLine(line, 0, 128);
+						// next line
+						i = in.readLine(line, 0, 128);
+						// blank line
+						i = in.readLine(line, 0, 128);
+						newLine = new String(line, 0, i);
+						// add the prefix to the filename
+
+						PrintWriter pw = null;
+						if (realSavePath.length() > 0) {
+							pw = new PrintWriter(new BufferedWriter(
+									new FileWriter((realSavePath == null ? ""
+											: realSavePath)
+											+ xmlFilename)));
+						} else {
+							xmlFilename = filenamePrefix + xmlFilename;
+							pw = new PrintWriter(new BufferedWriter(
+									new FileWriter((savePath == null ? ""
+											: savePath)
+											+ filename)));
+						}
+
+						while (i != -1 && !newLine.startsWith(boundary)) {
+							// the problem is the last line of the file content
+							// contains the new line character.
+							// So, we need to check if the current line is
+							// the last line.
+							i = in.readLine(line, 0, 128);
+							if ((i == boundaryLength + 2 || i == boundaryLength + 4) // +
+																						// 4
+																						// is
+																						// eof
+									&& (new String(line, 0, i)
+											.startsWith(boundary))) {
+
+								pw.print(newLine.substring(0,
+										newLine.length() - 2));
+							} else {
+								pw.print(newLine);
+							}
+
+							newLine = new String(line, 0, i);
+						}
+						pw.close();
+
+					}
+
+				}
+				
 				if (newLine.contains("processFile")) {
 					// we upload files not config docuemnts. Therefore store is
 					// somewhere else.
