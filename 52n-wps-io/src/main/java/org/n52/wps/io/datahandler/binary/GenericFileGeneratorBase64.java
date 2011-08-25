4a04bcc239379687401bb0e440667ad01fb16ad6
matthias-mueller <matthias_mueller@tu-dresden.de>
2011-08-25 15:48:59 +0000

    - added GenericFileParser/Generator with Base64 support (UNTESTED)
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@1941 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-io/src/main/java/org/n52/wps/io/datahandler/binary/GenericFileGeneratorBase64.java
@@ -0,0 +1,162 @@
+/***************************************************************
+Copyright © 2009 52°North Initiative for Geospatial Open Source Software GmbH
+
+ Author: Matthias Mueller, TU Dresden
+
+ Contact: Andreas Wytzisk, 
+ 52°North Initiative for Geospatial Open Source SoftwareGmbH, 
+ Martin-Luther-King-Weg 24,
+ 48155 Muenster, Germany, 
+ info@52north.org
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; even without the implied WARRANTY OF
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program (see gnu-gpl v2.txt). If not, write to
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA 02111-1307, USA or visit the Free
+ Software Foundation’s web page, http://www.fsf.org.
+
+ ***************************************************************/
+
+package org.n52.wps.io.datahandler.binary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import org.apache.commons.codec.binary.Base64InputStream;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.n52.wps.io.data.GenericFileData;
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.data.binding.complex.GenericFileDataBinding;
+
+public class GenericFileGeneratorBase64 {
+	private File tempfile;
+	
+	private static Logger LOGGER = Logger.getLogger(GenericFileGeneratorBase64.class);
+	
+	public boolean isSupportedSchema(String schema) {
+		boolean ret = false;
+		if(schema == null){
+			ret = true;
+		}
+		else {
+			if (schema.isEmpty()){
+				ret = true;
+			}
+		}
+		return ret;
+	}
+
+	
+
+	public void writeToStream(IData outputData, OutputStream outputStream) {
+		
+		
+		if(!(outputData instanceof GenericFileDataBinding)){
+			if(outputData==null){
+				throw new RuntimeException("GenericFileGenerator writer does not support incoming datatype.");
+			}else{
+				throw new RuntimeException("GenericFileGenerator writer does not support incoming datatype. Datatyüe is " + outputData.getClass());
+			}
+		}
+		LOGGER.info("Generating tempfile ...");
+		InputStream theStream = new Base64InputStream(((GenericFileDataBinding)outputData).getPayload().getDataStream(), true);
+		
+		try {
+			IOUtils.copy(theStream, outputStream);
+			theStream.close();
+			LOGGER.info("Tempfile generated!");
+			System.gc();
+		} catch (Exception e) {
+			LOGGER.error(e);
+			throw new RuntimeException(e);
+		}
+	}
+	
+
+	public OutputStream generate(IData data) {
+		LargeBufferStream outputStream = new LargeBufferStream();
+
+		
+		if(!(data instanceof GenericFileDataBinding)){
+			throw new RuntimeException("GenericFileGenerator does not support incoming datatype");
+		}
+		
+		InputStream theStream = new Base64InputStream(((GenericFileDataBinding)data).getPayload().getDataStream(),true);
+		
+		try {
+			IOUtils.copy(theStream, outputStream);
+			theStream.close();
+			System.gc();
+		} catch (Exception e) {
+			LOGGER.error(e);
+			throw new RuntimeException(e);
+		}
+		return outputStream;
+		
+	}
+	
+	
+	public File generateFile(IData data, String mimeType) {
+		
+		if(!(data instanceof GenericFileDataBinding)){
+			throw new RuntimeException("GenericFileGenerator does not support incoming datatype");
+		}
+		
+		GenericFileData currentFD = (GenericFileData)data.getPayload();
+		
+		//check for necessary conversions
+		if (currentFD.getMimeType() != mimeType)
+			data = new GenericFileDataBinding(convertFile(currentFD));
+		
+		//create a tempfile for the output
+		tempfile = new File("tempFile" + UUID.randomUUID() + ".temp");
+		try {
+			OutputStream outputStream = new FileOutputStream(tempfile);
+			writeToStream(data, outputStream);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tempfile;
+	}
+	
+	
+	public Class[] getSupportedInternalInputDataType() {
+		Class[] supportedClasses = {GenericFileDataBinding.class};
+		return supportedClasses;
+	}
+	
+	protected void finalize(){
+		if(tempfile.delete())
+			LOGGER.info("Generator tempfile successfully deleted!");
+		else
+			LOGGER.info("Generator tempfile could not be deleted :-(");
+		
+		//just to be safe - call the destructor from the superior class
+		try {
+			super.finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private GenericFileData convertFile (GenericFileData inputFile){
+		//not implemented
+		return null;
+	}
+}
