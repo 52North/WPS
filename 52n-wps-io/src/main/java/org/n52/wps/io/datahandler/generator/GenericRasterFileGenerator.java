ac03b5ec571d949722888f1acf4fbcad8a2be748
BSC-52n <dev-null@52north.org>
2012-02-01 10:28:29 +0000

    specific generic raster parser and generators added
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@2117 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-io/src/main/java/org/n52/wps/io/datahandler/generator/GenericRasterFileGenerator.java
@@ -0,0 +1,68 @@
+/***************************************************************
+Copyright © 2011 52°North Initiative for Geospatial Open Source Software GmbH
+
+ Author: Matthias Mueller, TU Dresden
+
+ Contact: Andreas Wytzisk, 
+ 52°North Initiative for Geospatial Open Source SoftwareGmbH, 
+ Martin-Luther-King-Weg 24,
+ 48155 Muenster, Germany, 
+ info@52north.org
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; even without the implied WARRANTY OF
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program (see gnu-gpl v2.txt). If not, write to
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA 02111-1307, USA or visit the Free
+ Software Foundation’s web page, http://www.fsf.org.
+
+ ***************************************************************/
+
+package org.n52.wps.io.datahandler.generator;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.data.GenericFileData;
+import org.n52.wps.io.data.binding.complex.GenericFileDataBinding;
+
+public class GenericRasterFileGenerator extends AbstractGenerator {
+	
+	public GenericRasterFileGenerator (){
+		super();
+		supportedIDataTypes.add(GenericFileDataBinding.class);
+	}
+	
+	public InputStream generateStream(IData data, String mimeType, String schema) throws IOException {
+		
+//		// check for correct request before returning the stream
+//		if (!(this.isSupportedGenerate(data.getSupportedClass(), mimeType, schema))){
+//			throw new IOException("I don't support the incoming datatype");
+//		}
+		
+		InputStream theStream = ((GenericFileDataBinding)data).getPayload().getDataStream();
+		return theStream;
+	}
+	
+	/**
+	 * conversion method to support translation of output formats
+	 * TODO: implement logic
+	 * 
+	 * @param inputFile
+	 * @return  
+	 */
+	private GenericFileData convertFile (GenericFileData inputFile){
+		//not implemented
+		return null;
+	}
+	
+}
