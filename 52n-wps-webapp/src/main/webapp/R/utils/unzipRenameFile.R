0399464425c41211df6f47bdb3d1862650d874d7
MatthiasHinz <m.hinz@52north.org>
2012-06-28 15:33:26 +0000

    WPS4R webapp folders reorganized
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@2419 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-webapp/src/main/webapp/R/utils/unzipRenameFile.R
@@ -0,0 +1,40 @@
+# Function to unzip input files and rename them in R
+# Files which are not zipped will be just renamed
+# 
+# Author: Matthias Hinz
+###############################################################################
+
+unzipRename = function(file, name, ext){
+	t=unzip(file)
+	baseFileName = paste(name,ext,sep=".")
+	
+	if(length(t)==0){
+		file.rename(file, baseFileName)
+		return(baseFileName)
+	}
+	
+	for(i in t){
+		suffix = gsub("./","",i)
+		suffix = unlist(strsplit(i,"\\."))
+		if(length(suffix)>1){
+			suffix = suffix[length(suffix)]
+			suffix = paste(".",suffix,sep="")
+		}else suffix = ""
+		newName = paste(name,suffix,sep="")
+		file.rename(i, newName)
+	}
+	
+	return(baseFileName)
+}
+
+zipShp = function(file){
+	base = unlist(strsplit(file,"\\."))[1]
+	shx = paste(base,"shx", sep=".")
+	dbf = paste(base,"dbf", sep=".")
+	prj = paste(base,"prj", sep=".")
+	zip = paste(base,"zip", sep=".")
+	zip(zip, c(file,shx,dbf,prj))
+	if(zip %in% dir())
+		return(zip)
+	else return(NULL)
+}
