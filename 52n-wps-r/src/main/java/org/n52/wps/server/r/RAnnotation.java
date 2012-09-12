6938d350ac5ac009a43f9b7e45b8b4ce392e0f03
MatthiasHinz <m.hinz@52north.org>
2012-09-12 17:21:10 +0000

    [WPS4R] Fixed image format properties (encoding=null)
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@2603 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-r/src/main/java/org/n52/wps/server/r/RAnnotation.java
+++ 52n-wps-r/src/main/java/org/n52/wps/server/r/RAnnotation.java
@@ -193,12 +193,21 @@ public class RAnnotation {
                 "UTF-8"),
 
         // graphical data
-        GIF("gif", GenericFileDataConstants.MIME_TYPE_IMAGE_GIF, GenericFileDataBinding.class, true, null, "base64"), JPEG(
+        GIF("gif", GenericFileDataConstants.MIME_TYPE_IMAGE_GIF, GenericFileDataBinding.class, true, null, null), 
+        
+        JPEG(
                 "jpeg", GenericFileDataConstants.MIME_TYPE_IMAGE_JPEG, GenericFileDataBinding.class, true, null,
-                "base64"), JPEG2("jpg", GenericFileDataConstants.MIME_TYPE_IMAGE_JPEG, GenericFileDataBinding.class,
-                true, null, "base64"), PNG("png", GenericFileDataConstants.MIME_TYPE_IMAGE_PNG,
-                GenericFileDataBinding.class, true, null, "base64"), TIFF("tiff",
-                GenericFileDataConstants.MIME_TYPE_TIFF, GenericFileDataBinding.class, true, null, "base64"),
+                null), 
+                
+           JPEG2("jpg", GenericFileDataConstants.MIME_TYPE_IMAGE_JPEG, GenericFileDataBinding.class,
+                true, null, null), 
+                
+           PNG("png", GenericFileDataConstants.MIME_TYPE_IMAGE_PNG,
+                GenericFileDataBinding.class, true, null, null), 
+                
+                
+            TIFF("tiff",
+                GenericFileDataConstants.MIME_TYPE_TIFF, GenericFileDataBinding.class, true, null, null),
 
         // file data and xml:
         TEXT_PLAIN("text", GenericFileDataConstants.MIME_TYPE_PLAIN_TEXT, GenericFileDataBinding.class, true), TEXT_XML(
