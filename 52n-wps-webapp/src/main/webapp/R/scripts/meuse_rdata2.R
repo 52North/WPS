43fba7f1526fca25b2978036ad547105ee6c281d
MatthiasHinz <m.hinz@52north.org>
2013-01-10 11:26:51 +0000

    RData test scripts for WPS4R + execute request
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@2884 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

---
+++ 52n-wps-webapp/src/main/webapp/R/scripts/meuse_rdata2.R
@@ -0,0 +1,17 @@
+#wps.des: title=Script that returns meuse data as rdata-files, abstract=The meuse river data set is contained in the sp-package of R - see package information;
+#wps.in: input, integer, dummy input, value=0;
+
+require(sp)
+loadMeuse(river=TRUE)
+summary(meuse)
+summary(meuse.grid)
+summary(meuse.riv)
+
+
+#wps.out: workspace, rdata;
+workspace="workspace.RData"
+save.image(workspace)
+
+
+
+
