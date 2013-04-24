d1bc9e41512db82c8b6d4e57b566b57b09e10008
nuest <d.nuest@52north.org>
2013-04-24 14:15:00 +0000

    renamed random script, fixed sessionInfo jsp
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/trunk/WPS@3193 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-webapp/src/main/webapp/R/scripts/uniform.R
+++ 52n-wps-webapp/src/main/webapp/R/scripts/uniform.R
@@ -1,11 +1,12 @@
-# wps.des: id = R_andom, title = Random number generator, 
-# abstract = Generates random numbers for uniform distribution; 
+# wps.des: id = R_andom, title = Random number generator, version = 42,
+# 	abstract = Generates random numbers for uniform distribution; 
+
 # wps.in: min, double, Minimum, All outcomes are larger than min, value = 0; 
 # wps.in: max, double, Maximum, All outcomes are smaller than max, value = 1;
 # wps.in: n, integer, ammount of random numbers, value = 100;  
-# random number: 
 x = runif(n, min=min, max=max)
 
-# wps.out: output, text, Random number list, Textfile containing n random numbers in one column;
+# wps.out: output, text, Random number list,
+#	Text file with list of n random numbers in one column;
 output = "random_out"
 write.table(x, output)
--- 52n-wps-webapp/src/main/webapp/R/sessioninfo.jsp
+++ 52n-wps-webapp/src/main/webapp/R/sessioninfo.jsp
@@ -1,4 +1,4 @@
 <%@ page contentType="text/plain" pageEncoding="UTF-8"%>
 <%@ page import="org.n52.wps.server.r.R_Config"%><%@ page
 	isErrorPage="false"%>
-<%=R_Config.getInstance().getSessionInfo()%>
+<%=R_Config.getInstance().getCurrentSessionInfo()%>
