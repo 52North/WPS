9046d860fbdf3005000fbc0784a7e3c0f42c1d2c
tkunicki-usgs <tkunicki@usgs.gov>
2013-04-26 03:15:44 +0000

    strengthened unit test for PropertyUtil
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/branches/WPS-3.1.0-maintenance@3205 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-commons/src/test/java/org/n52/wps/commons/PropertyUtilTest.java
+++ 52n-wps-commons/src/test/java/org/n52/wps/commons/PropertyUtilTest.java
@@ -27,16 +27,25 @@ public class PropertyUtilTest {
     static final String KEY_TEST_PERIOD = "sample.period";
     
     static final boolean DEFAULT_BOOLEAN = false;
-    static final long   DEFAULT_LONG = 1l;
-    static final double DEFAULT_DOUBLE = 1d;
-    static final String DEFAULT_STRING = "default";
-    static final String DEFAULT_PERIOD = "P7D";
-    static final long   DEFAULT_PERIOD_MS = Period.days(7).toStandardDuration().getMillis();
+    static final long    DEFAULT_LONG = 1l;
+    static final double  DEFAULT_DOUBLE = 1d;
+    static final String  DEFAULT_STRING = "default";
+    static final String  DEFAULT_PERIOD = "P1D";
+    static final long    DEFAULT_PERIOD_MS = Period.days(1).toStandardDuration().getMillis();
     
     static final boolean CONFIG_BOOLEAN = true;
-    static final boolean CONFIG_LONG = true;
-    static final boolean CONFIG_DOUBLE = true;
-    static final boolean CONFIG_STRING = true;
+    static final long    CONFIG_LONG = 2l;
+    static final double  CONFIG_DOUBLE = 2d;
+    static final String  CONFIG_STRING = "config";
+    static final String  CONFIG_PERIOD = "P2D";
+    static final long    CONFIG_PERIOD_MS = Period.days(2).toStandardDuration().getMillis();
+    
+    static final boolean SYSTEM_BOOLEAN = true;
+    static final long    SYSTEM_LONG = 3l;
+    static final double  SYSTEM_DOUBLE = 3d;
+    static final String  SYSTEM_STRING = "system";
+    static final String  SYSTEM_PERIOD = "P3D";
+    static final long    SYSTEM_PERIOD_MS = Period.days(3).toStandardDuration().getMillis();
     
     @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
     
@@ -87,12 +96,12 @@ public class PropertyUtilTest {
         
         setValidSystemProperties();
         try {
-            System.out.println("Verify system property values passed through");
-            assertThat("extractBoolean", cu.extractBoolean(KEY_TEST_BOOLEAN, DEFAULT_BOOLEAN), equalTo(true));
-            assertThat("extractLong", cu.extractLong(KEY_TEST_LONG, DEFAULT_LONG), equalTo(3l));
-            assertThat("extractDouble", cu.extractDouble(KEY_TEST_DOUBLE, DEFAULT_DOUBLE), equalTo(3d));
-            assertThat("extractString", cu.extractString(KEY_TEST_STRING, DEFAULT_STRING), equalTo("system"));
-            assertThat("extractPeriodAsMillis", cu.extractPeriodAsMillis(KEY_TEST_PERIOD, DEFAULT_PERIOD_MS), equalTo(Period.days(3).toStandardDuration().getMillis()));
+            System.out.println("Verify system property values are used if present");
+            assertThat("extractBoolean", cu.extractBoolean(KEY_TEST_BOOLEAN, DEFAULT_BOOLEAN), equalTo(SYSTEM_BOOLEAN));
+            assertThat("extractLong", cu.extractLong(KEY_TEST_LONG, DEFAULT_LONG), equalTo(SYSTEM_LONG));
+            assertThat("extractDouble", cu.extractDouble(KEY_TEST_DOUBLE, DEFAULT_DOUBLE), equalTo(SYSTEM_DOUBLE));
+            assertThat("extractString", cu.extractString(KEY_TEST_STRING, DEFAULT_STRING), equalTo(SYSTEM_STRING));
+            assertThat("extractPeriodAsMillis", cu.extractPeriodAsMillis(KEY_TEST_PERIOD, DEFAULT_PERIOD_MS), equalTo(SYSTEM_PERIOD_MS));
         } finally {
             clearSystemProperties();
         }
@@ -105,21 +114,36 @@ public class PropertyUtilTest {
         setValidSystemProperties();
         try {
             System.out.println("Verify system property values preferred over config property values");
-            assertThat("extractBoolean", cu.extractBoolean(KEY_TEST_BOOLEAN, DEFAULT_BOOLEAN), equalTo(true));
-            assertThat("extractLong", cu.extractLong(KEY_TEST_LONG, DEFAULT_LONG), equalTo(3l));
-            assertThat("extractDouble", cu.extractDouble(KEY_TEST_DOUBLE, DEFAULT_DOUBLE), equalTo(3d));
-            assertThat("extractString", cu.extractString(KEY_TEST_STRING, DEFAULT_STRING), equalTo("system"));
-            assertThat("extractPeriodAsMillis", cu.extractPeriodAsMillis(KEY_TEST_PERIOD, DEFAULT_PERIOD_MS), equalTo(Period.days(3).toStandardDuration().getMillis()));
+            assertThat("extractBoolean", cu.extractBoolean(KEY_TEST_BOOLEAN, DEFAULT_BOOLEAN), equalTo(SYSTEM_BOOLEAN));
+            assertThat("extractLong", cu.extractLong(KEY_TEST_LONG, DEFAULT_LONG), equalTo(SYSTEM_LONG));
+            assertThat("extractDouble", cu.extractDouble(KEY_TEST_DOUBLE, DEFAULT_DOUBLE), equalTo(SYSTEM_DOUBLE));
+            assertThat("extractString", cu.extractString(KEY_TEST_STRING, DEFAULT_STRING), equalTo(SYSTEM_STRING));
+            assertThat("extractPeriodAsMillis", cu.extractPeriodAsMillis(KEY_TEST_PERIOD, DEFAULT_PERIOD_MS), equalTo(SYSTEM_PERIOD_MS));
         } finally {
             clearSystemProperties();
         }
     }
     
     @Test
-    public void testExtractValid_PropertiesInactive_NoSystemProperties() {
+    public void testExtractValid_ConfigProperties_NoSystemProperties() {
+        
+        PropertyUtil cu = new PropertyUtil(getValidMockProperties(true));
+        
+        System.out.println("Verify config property values are used if system aren't present");
+        assertThat("extractBoolean", cu.extractBoolean(KEY_TEST_BOOLEAN, DEFAULT_BOOLEAN), equalTo(CONFIG_BOOLEAN));
+        assertThat("extractLong", cu.extractLong(KEY_TEST_LONG, DEFAULT_LONG), equalTo(CONFIG_LONG));
+        assertThat("extractDouble", cu.extractDouble(KEY_TEST_DOUBLE, DEFAULT_DOUBLE), equalTo(CONFIG_DOUBLE));
+        assertThat("extractString", cu.extractString(KEY_TEST_STRING, DEFAULT_STRING), equalTo(CONFIG_STRING));
+        assertThat("extractPeriodAsMillis", cu.extractPeriodAsMillis(KEY_TEST_PERIOD, DEFAULT_PERIOD_MS), equalTo(CONFIG_PERIOD_MS));
+        
+    }
+    
+    @Test
+    public void testExtractValid_ConfigPropertiesInactive_NoSystemProperties() {
         
         PropertyUtil cu = new PropertyUtil(getValidMockProperties(false));
         
+        System.out.println("Verify config property values ignored when inactive (defaults are used in absense of system properties)");
         assertThat("extractBoolean", cu.extractBoolean(KEY_TEST_BOOLEAN, DEFAULT_BOOLEAN), equalTo(DEFAULT_BOOLEAN));
         assertThat("extractLong", cu.extractLong(KEY_TEST_LONG, DEFAULT_LONG), equalTo(DEFAULT_LONG));
         assertThat("extractDouble", cu.extractDouble(KEY_TEST_DOUBLE, DEFAULT_DOUBLE), equalTo(DEFAULT_DOUBLE));
@@ -133,23 +157,23 @@ public class PropertyUtilTest {
         context.checking(new Expectations() {{
             allowing (properties[0]).getName(); will(returnValue(KEY_TEST_BOOLEAN));
             allowing (properties[0]).getActive(); will(returnValue(active));
-            allowing (properties[0]).getStringValue(); will(returnValue("true"));
+            allowing (properties[0]).getStringValue(); will(returnValue(Boolean.toString(CONFIG_BOOLEAN)));
             
             allowing (properties[1]).getName(); will(returnValue(KEY_TEST_LONG));
             allowing (properties[1]).getActive(); will(returnValue(active));
-            allowing (properties[1]).getStringValue(); will(returnValue(Long.toString(2l)));
+            allowing (properties[1]).getStringValue(); will(returnValue(Long.toString(CONFIG_LONG)));
             
             allowing (properties[2]).getName(); will(returnValue(KEY_TEST_DOUBLE));
             allowing (properties[2]).getActive(); will(returnValue(active));
-            allowing (properties[2]).getStringValue(); will(returnValue(Double.toString(2d)));
+            allowing (properties[2]).getStringValue(); will(returnValue(Double.toString(CONFIG_DOUBLE)));
             
             allowing (properties[3]).getName(); will(returnValue(KEY_TEST_STRING));
             allowing (properties[3]).getActive(); will(returnValue(active));
-            allowing (properties[3]).getStringValue(); will(returnValue("config"));
+            allowing (properties[3]).getStringValue(); will(returnValue(CONFIG_STRING));
             
             allowing (properties[4]).getName(); will(returnValue(KEY_TEST_PERIOD));
             allowing (properties[4]).getActive(); will(returnValue(active));
-            allowing (properties[4]).getStringValue(); will(returnValue("P2D"));
+            allowing (properties[4]).getStringValue(); will(returnValue(CONFIG_PERIOD));
         }});
         
         return properties;
@@ -157,11 +181,11 @@ public class PropertyUtilTest {
     
     private void setValidSystemProperties() {
         Joiner dot = Joiner.on('.');
-        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_BOOLEAN), "true");
-        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_LONG), Long.toString(3l));
-        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_DOUBLE), Double.toString(3d));
-        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_STRING), "system");
-        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_PERIOD), "P3D");
+        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_BOOLEAN), Boolean.toString(SYSTEM_BOOLEAN));
+        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_LONG), Long.toString(SYSTEM_LONG));
+        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_DOUBLE), Double.toString(SYSTEM_DOUBLE));
+        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_STRING), SYSTEM_STRING);
+        System.setProperty(dot.join(KEY_TEST_ROOT, KEY_TEST_PERIOD), SYSTEM_PERIOD);
     }
     
     private void clearSystemProperties() {
