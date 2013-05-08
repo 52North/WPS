21d3cb2d5870426a89152db3025afd5a1e7fc4c3
bpross-52n <b.pross@52north.org>
2013-05-08 12:30:14 +0000

    Revert r3210.
    git-svn-id: https://svn.52north.org/svn/geoprocessing/main/WPS/branches/WPS-3.1.0-maintenance@3224 15ebfbcb-1bbe-4f7e-88aa-3b0fa5d666c2

--- 52n-wps-server/src/main/java/org/n52/wps/server/request/strategy/DefaultReferenceStrategy.java
+++ 52n-wps-server/src/main/java/org/n52/wps/server/request/strategy/DefaultReferenceStrategy.java
@@ -103,7 +103,7 @@ public class DefaultReferenceStrategy implements IReferenceStrategy{
 	 * TODO: add support for autoretry, proxy
 	 */
 	private ReferenceInputStream httpGet(final String dataURLString, final String mimeType) throws IOException {
-		HttpClient backend = HttpClientFactory.getDefaultClient();
+		HttpClient backend = new DefaultHttpClient();
 		DecompressingHttpClient httpclient = new DecompressingHttpClient(backend);
 		
 		HttpGet httpget = new HttpGet(dataURLString);
@@ -121,7 +121,7 @@ public class DefaultReferenceStrategy implements IReferenceStrategy{
 	 * TODO: add support for autoretry, proxy
 	 */
 	private ReferenceInputStream httpPost(final String dataURLString, final String body, final String mimeType) throws IOException {
-		HttpClient backend = HttpClientFactory.getDefaultClient();
+		HttpClient backend = new DefaultHttpClient();
 		
 		DecompressingHttpClient httpclient = new DecompressingHttpClient(backend);
 		
