507c2c1bbd3b5f6a376ed8bbd7752ed46cc166c7
Khalid Alqinyah <k.alqinyah@gmail.com>
2013-07-18 06:27:36 +0300

    Created Tables For ConfigurationEntry And AlgorithmEntry
    Created tables definitions to hold configuration entries values and
    algorithm entries status.

---
+++ 52n-wps-webapp/src/main/resources/db/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS configurationentry (
+  entry_key varchar(255),
+  configuration_module varchar(255),
+  configuration_value varchar(255) DEFAULT NULL,
+  PRIMARY KEY (entry_key, configuration_module),
+);
+
+CREATE TABLE IF NOT EXISTS algorithmnentry (
+  algorithm_name varchar(255),
+  configuration_module varchar(255),
+  active boolean DEFAULT true,
+  PRIMARY KEY (algorithm_name, configuration_module),
+);
