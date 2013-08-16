a98de65d6da482c71d3d7cd794a24e2dabb0f0c0
Khalid Alqinyah <k.alqinyah@gmail.com>
2013-08-16 18:02:51 +0800

    Handled 403 errors

--- 52n-wps-webapp/src/main/java/org/n52/wps/webapp/web/ErrorController.java
+++ 52n-wps-webapp/src/main/java/org/n52/wps/webapp/web/ErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorController {
-    @RequestMapping(value={"/error", "/404"})
+    @RequestMapping(value={"/error", "/404", "/403"})
     public String handle(HttpServletRequest request, Model model) {
         model.addAttribute("errorCode", request.getAttribute("javax.servlet.error.status_code"));
         model.addAttribute("errorMessage", request.getAttribute("javax.servlet.error.message"));
