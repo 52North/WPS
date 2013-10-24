a38e71349bb9ba9784bbecddd94068e26194ccc3
Ivan Suftin <isuftin@usgs.gov>
2013-10-24 14:31:03 -0500

    Updated license to match what TKunicki did with FlatFileDatabase

--- 52n-wps-database/src/main/java/org/n52/wps/server/database/PostgresDatabase.java
+++ 52n-wps-database/src/main/java/org/n52/wps/server/database/PostgresDatabase.java
@@ -1,29 +1,31 @@
-/**
- * *************************************************************
- * This implementation provides a framework to publish processes to the web
- * through the OGC Web Processing Service interface. The framework is extensible
- * in terms of processes and data handlers. It is compliant to the WPS version
- * 0.4.0 (OGC 05-007r4).  *
- * Copyright (C) 2007 by con terra GmbH
- *
- * Author(s): Ivan Suftin
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program (see gnu-gpl v2.txt); if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
- * visit the web page of the Free Software Foundation, http://www.fsf.org.
- *
- **************************************************************
- */
+/*****************************************************************
+Copyright © 2012 52¡North Initiative for Geospatial Open Source Software GmbH
+
+ Author: Ivan Suftin, USGS
+ 
+
+ Contact: Andreas Wytzisk, 
+ 52¡North Initiative for Geospatial Open Source SoftwareGmbH, 
+ Martin-Luther-King-Weg 24,
+ 48155 Muenster, Germany, 
+ info@52north.org
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; even without the implied WARRANTY OF
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program (see gnu-gpl v2.txt). If not, write to
+ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA 02111-1307, USA or visit the Free
+ Software Foundation?s web page, http://www.fsf.org.
+
+ ***************************************************************/
 package org.n52.wps.server.database;
 
 import com.google.common.base.Joiner;
