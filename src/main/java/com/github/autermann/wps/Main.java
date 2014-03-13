846421a3fd29bed1f39299c36df27043916dea19
Christian Autermann <christian@autermann.org>
2014-03-13 17:14:19 +0100

    use fluent interface

--- src/main/java/com/github/autermann/wps/Main.java
+++ src/main/java/com/github/autermann/wps/Main.java
@@ -3,19 +3,18 @@ package com.github.autermann.wps;
 import com.github.autermann.wps.commons.WPS;
 import com.github.autermann.wps.matlab.MatlabAlgorithmRepository;
 import com.github.autermann.wps.matlab.MatlabFileHandler;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.SetMultimap;
+import com.google.common.collect.ImmutableMultimap;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        WPS wps = new WPS("localhost", 12121);
-        SetMultimap<String, String> properties = HashMultimap.create();
-        properties.put(MatlabAlgorithmRepository.CONFIG_PROPERTY,
-                       "/home/auti/Source/Lake-Analyzer/WPS/lakeAnalyzer.yaml");
-        wps.addAlgorithmRepository(MatlabAlgorithmRepository.class, properties);
-        wps.addGenerator(MatlabFileHandler.class);
-        wps.addParser(MatlabFileHandler.class);
-        wps.start();
+    public static void main(String[] args)
+            throws Exception {
+        new WPS("localhost", 12121)
+                .addAlgorithmRepository(MatlabAlgorithmRepository.class, ImmutableMultimap
+                        .of(MatlabAlgorithmRepository.CONFIG_PROPERTY,
+                            "/home/auti/Source/Lake-Analyzer/WPS/lakeAnalyzer.yaml"))
+                .addGenerator(MatlabFileHandler.class)
+                .addParser(MatlabFileHandler.class)
+                .start();
     }
 }
