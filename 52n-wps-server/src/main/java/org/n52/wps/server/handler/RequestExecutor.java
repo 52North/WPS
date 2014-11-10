da65b899c4469d328fa6d22cd93372f00cbd633d
Benjamin Pross <b.pross@52north.org>
2014-11-10 13:11:02 +0100

    Use config xml values for threadpool config

--- 52n-wps-server/src/main/java/org/n52/wps/server/handler/RequestExecutor.java
+++ 52n-wps-server/src/main/java/org/n52/wps/server/handler/RequestExecutor.java
@@ -32,6 +32,9 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.n52.wps.ServerDocument.Server;
+import org.n52.wps.commons.WPSConfig;
+
 /**
  * After the client Request is accepted, it should be executed. To prevent
  * resource-exhaustion, this ThreadPoolExecutor stores the Requests in a queue,
@@ -45,20 +48,15 @@ import java.util.concurrent.TimeUnit;
  */
 public class RequestExecutor extends ThreadPoolExecutor {
 
-	public static final int MIN_POOL_SIZE = 10;
-	public static final int MAX_POOL_SIZE = 20;
-	// When the number of threads is greater than the MIN_POOL_SIZE, this is the maximum 
-	// time that excess idle threads will wait for new tasks before terminating.
-	public static final int KEEP_ALIVE_SECONDS = 1000;
-	public static final int MAX_QUEUED_TASKS = 100;
-
+	public static Server serverConfig = WPSConfig.getInstance().getWPSConfig().getServer();
+	
 	/**
 	 * Create a RequestExecutor.
 	 */
 	public RequestExecutor() {
-		super(MIN_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_SECONDS,
+		super(serverConfig.getMinPoolSize().intValue(), serverConfig.getMaxPoolSize().intValue(), serverConfig.getKeepAliveSeconds().intValue(),
 				TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(
-						MAX_QUEUED_TASKS));
+						serverConfig.getMaxQueuedTasks().intValue()));
 	}
 
 }
