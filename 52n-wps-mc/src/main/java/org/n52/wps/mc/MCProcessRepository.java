b54bcd8733f33ee8ff73fcd7cd13cc8fd2cb5d41
Matthias Müller <matthias_mueller@tu-dresden.de>
2015-01-14 13:27:05 +0100

    Fixed broken repository load routine Updated to new processor configuration

--- 52n-wps-mc/src/main/java/org/n52/wps/mc/MCProcessRepository.java
+++ 52n-wps-mc/src/main/java/org/n52/wps/mc/MCProcessRepository.java
@@ -1,5 +1,5 @@
 /**
- * ﻿Copyright (C) 2012 - 2014 52°North Initiative for Geospatial Open Source
+ * ﻿Copyright (C) 2012 - 2015 52°North Initiative for Geospatial Open Source
  * Software GmbH
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -33,6 +33,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 import net.opengis.wps.x100.ProcessDescriptionType;
 
@@ -67,33 +68,51 @@ public class MCProcessRepository implements IAlgorithmRepository {
 
 	// use the GlobalRepoManager from mc-runtime for the process inventory
 	private GlobalRepositoryManager rm = GlobalRepositoryManager.getInstance();
-
+	
 	// valid functionIDs
-	private String[] validFunctionIDs = null;
-
-	private static Logger logger = LoggerFactory.getLogger(MCProcessRepository.class);
+	private volatile Collection<String> supportedFunctionIDs = Collections.emptyList();
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(MCProcessRepository.class);
 
 	public MCProcessRepository() {
 		super();
-
+		
+		// register a change listener with the GlobalRepositoryManager
+		// to listen for content updates
+		rm.addRepositoryChangeListener(new RepositoryChangeListener() {
+			
+			@Override
+			public void onRepositoryUpdate(IMovingCodeRepository updatedRepo) {
+				// trigger a content update of this repo
+				updateContent();
+				// and notify the WPS framework to trigger a Capabilities update
+				LOGGER.info("Moving Code repository content has changed. Capabilities update required.");
+				WPSConfig.getInstance().firePropertyChange(WPSConfig.WPSCAPABILITIES_SKELETON_PROPERTY_EVENT_NAME);
+			}
+		});
+		
 		// check if the repository is active
 		if (WPSConfig.getInstance().isRepositoryActive(this.getClass().getCanonicalName())) {
-			
+
 			// trigger remote repo init in separate thread
 			Thread tLoadRemote = new LoadRepoThread();
 			tLoadRemote.start();
 		}
 		else {
-			logger.debug("MCProcessRepository is inactive.");
+			LOGGER.debug("MCProcessRepository is inactive.");
 		}
 	}
 
 	@Override
 	public Collection<String> getAlgorithmNames() {
+		return supportedFunctionIDs;
+	}
+
+	private synchronized void updateContent(){
 
 		// run this block if validFunctionIDs are not yet available
 		// checks which available functions can be executed with current configuration
-		if (validFunctionIDs == null) {
+
 			// 1. get all available functionIDs
 			String[] fids = rm.getFunctionIDs();
 			ArrayList<String> exFIDs = new ArrayList<String>();
@@ -102,7 +121,8 @@ public class MCProcessRepository implements IAlgorithmRepository {
 			for (String currentFID : fids) {
 				// 2.a retrieve implementing packages
 				MovingCodePackage[] mcps = rm.getPackageByFunction(currentFID);
-				// 2.b check whether one of them can be executed
+				// 2.b check whether each one of them can be executed
+				//     by the current processor configuration
 				for (MovingCodePackage currentMCP : mcps) {
 					boolean supported = ProcessorFactory.getInstance().supportsPackage(currentMCP);
 					if (supported) {
@@ -111,11 +131,8 @@ public class MCProcessRepository implements IAlgorithmRepository {
 					}
 				}
 			}
-
-			validFunctionIDs = exFIDs.toArray(new String[exFIDs.size()]);
-		}
-
-		return Arrays.asList(validFunctionIDs);
+			
+			supportedFunctionIDs = exFIDs;
 	}
 
 	@Override
@@ -159,17 +176,17 @@ public class MCProcessRepository implements IAlgorithmRepository {
 					try {
 						URL repoURL = new URL(property.getStringValue());
 						rm.addRepository(repoURL);
-						logger.info("Added MovingCode Repository: " + property.getName() + " - "
+						LOGGER.info("Added MovingCode Repository: " + property.getName() + " - "
 								+ property.getStringValue());
 					}
 					catch (MalformedURLException e) {
-						logger.warn("MovingCode Repository is not a valid URL: " + property.getName() + " - "
+						LOGGER.warn("MovingCode Repository is not a valid URL: " + property.getName() + " - "
 								+ property.getStringValue());
 					}
 					catch (Exception e) {
 						// catch any unexpected error; if we get here this is probably an indication for a
 						// bug/flaw in mc-runtime ...
-						logger.error("Error invoking MovingCode Runtime for feed URL : " + property.getName() + " - "
+						LOGGER.error("Error invoking MovingCode Runtime for feed URL : " + property.getName() + " - "
 								+ property.getStringValue());
 					}
 
@@ -183,47 +200,21 @@ public class MCProcessRepository implements IAlgorithmRepository {
 					try {
 						String repoFolder = property.getStringValue();
 						rm.addLocalZipPackageRepository(repoFolder);
-						logger.info("Added MovingCode Repository: " + property.getName() + " - "
+						LOGGER.info("Added MovingCode Repository: " + property.getName() + " - "
 								+ property.getStringValue());
 					}
 					catch (Exception e) {
 						// catch any unexpected error; if we get here this is probably an indication for a
 						// bug/flaw in mc-runtime ...
-						logger.error("Error invoking MovingCode Runtime for feed URL : " + property.getName() + " - "
+						LOGGER.error("Error invoking MovingCode Runtime for feed URL : " + property.getName() + " - "
 								+ property.getStringValue());
 					}
 
 				}
 			}
-
-			// add a change listener to the GlobalRepositoryManager rm
-			rm.addRepositoryChangeListener(new RepositoryChangeListener() {
-				@Override
-				public void onRepositoryUpdate(IMovingCodeRepository updatedRepo) {
-					// clear validFunctionIDs
-					validFunctionIDs = null;
-
-					// trigger Capabilities update
-					logger.info("Moving Code repository content has changed. Capabilities update required.");
-					WPSConfig.getInstance().firePropertyChange(WPSConfig.WPSCAPABILITIES_SKELETON_PROPERTY_EVENT_NAME);
-				}
-			});
 			
-			// trigger Capabilities update
-			logger.info("Moving Code repositories have been loaded. Capabilities update required.");
-			WPSConfig.getInstance().firePropertyChange(WPSConfig.WPSCAPABILITIES_SKELETON_PROPERTY_EVENT_NAME);
-
-// 			-- old code --			
-//			try {
-//				CapabilitiesConfiguration.reloadSkeleton();
-//			} catch (XmlException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
 			
+			LOGGER.info("The following repositories have been loaded:\n{}", Arrays.toString(rm.getRegisteredRepositories()));
 		}
 	}
 
