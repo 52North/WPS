c72230cc874524d18bb259a7a8690d2777f6fad7
Henning Bredel <h.bredel@52north.org>
2015-09-22 13:53:24 +0200

    some formatting

--- 52n-wps-webapp/src/main/java/org/n52/wps/webapp/LandingPageController.java
+++ 52n-wps-webapp/src/main/java/org/n52/wps/webapp/LandingPageController.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2007-2015-09-22 13:21 52°North Initiative for Geospatial Open Source
+ * Copyright (C) 2007-2015 52°North Initiative for Geospatial Open Source
  * Software GmbH
  *
  * This program is free software; you can redistribute it and/or modify it
